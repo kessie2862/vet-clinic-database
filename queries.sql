ee3334cce37a474ff06fcda54d5a3b2b067ced6c
kessie <kayp3477@gmail.com>
2023-07-18 15:38:05 +0000

    Performed some queries

--- queries.sql
+++ queries.sql
@@ -27,3 +27,71 @@ WHERE name <> 'Gabumon';
 SELECT *
 FROM animals
 WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+-------------------------------------------------------------------
+BEGIN TRANSACTION;
+
+UPDATE animals SET species = 'unspecified';
+
+SELECT * FROM animals;
+
+ROLLBACK;
+
+SELECT * FROM animals;
+-------------------------------------------------------------------
+
+BEGIN TRANSACTION;
+
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+COMMIT;
+
+SELECT * FROM animals;
+-------------------------------------------------------------------
+
+BEGIN TRANSACTION;
+
+DELETE FROM animals;
+
+SELECT * FROM animals;
+
+ROLLBACK;
+
+SELECT * FROM animals;
+-------------------------------------------------------------------
+BEGIN TRANSACTION;
+
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT vet_savepoint;
+
+UPDATE animals SET weight_kg = weight_kg * -1;
+
+ROLLBACK TO SAVEPOINT vet_savepoint;
+
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+COMMIT;
+-------------------------------------------------------------------
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, COUNT(*) AS escape_count FROM animals WHERE escape_attempts > 0
+GROUP BY neutered
+ORDER BY escape_count DESC
+LIMIT 1;
+
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight
+FROM animals
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
+-------------------------------------------------------------------
